b2d5e1076fe6cbf9ae9583221136ac103cc252de
hkvijay54 <hkvijay54@gmail.com>
2021-12-19 03:07:43 +0530

    Prime or not

--- LogicalProgramProject/Program.cs
+++ LogicalProgramProject/Program.cs
@@ -1,27 +1,40 @@
 ﻿using System;
-// Perfect Number
+// Prime Number
 
+
+class Prime
+{
+    public static int Logic(int a)
+    {
+        int i;
+        for( i = 2; i <= a-1;i++)
+        {
+            if(a%i == 0)
+            {
+                return 0;
+            }
+        }
+        if(i == a)
+        {
+            return 1;
+        }
+        return 0;
+    }
+}
 class Program
 {
     static void Main(string[] args)
     {
-        int sum = 0;
         Console.WriteLine("Enter a number");
         int num = Convert.ToInt32(Console.ReadLine());
-        for(int i = 1; i < num; i++)
+        int res=Prime.Logic(num);
+        if(res == 0)
         {
-            if(num%i==0)
-            {
-                sum = sum + i;
-            }
-        }
-        if(sum == num)
-        {
-            Console.WriteLine("Entered number is Perfect Number " + sum);
+            Console.WriteLine("{0} is not a prime nuber", num);
         }
         else
         {
-            Console.WriteLine("Entered number not Perfect number "+sum);
+            Console.WriteLine("{0} is a prime number", num);
         }
     }
 }
