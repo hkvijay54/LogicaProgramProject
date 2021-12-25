8a6ae5093b1b941583d7353d4dedc02a461c6971
hkvijay54 <hkvijay54@gmail.com>
2021-12-25 20:49:02 +0530

    vending Machine

--- LogicalProgramProject/Program.cs
+++ LogicalProgramProject/Program.cs
@@ -5,29 +5,54 @@ using System.Text;
 using System.Linq;
 // coupon code problem
 
+
+class VendingMachine
+{
+    // function to count and
+    // print currency notes
+    public static void countCurrency(int amount)
+    {
+        int[] notes = new int[]{ 2000, 500, 200, 100,
+                     50, 20, 10, 5, 1 };
+        int[] noteCounter = new int[9];
+
+        // count notes using Greedy approach
+        for (int i = 0; i < 9; i++)
+        {
+            if (amount >= notes[i])
+            {
+                noteCounter[i] = amount / notes[i];
+                amount = amount - noteCounter[i] * notes[i];
+                Console.WriteLine(amount);
+            }
+        }
+
+        // Print notes
+        Console.WriteLine("currency count ->");
+        int[] notes1 = new int[]{ 2000, 500, 200, 100,
+                     50, 20, 10, 5, 1 };
+        int largestnote;
+        for (int i = 0; i < 9; i++)
+        {
+            if (noteCounter[i] != 0)
+            {
+                Console.WriteLine(notes[i] + " : " + noteCounter[i]);
+
+            }
+
+        }
+
+
+    }
+}
 class Program
 {
     static void Main(string[] args)
     {
-        var chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-        var stringChar = new char[6];
-        var random=new Random();
-
-        Console.WriteLine("How many tickets to generate?");
-        int number = Convert.ToInt32(Console.ReadLine());
-
-        while(number > 0)
-        {
-            for(int i = 0; i < stringChar.Length; i++)
-            {
-                stringChar[i]= chars[random.Next(chars.Length)];
-            }
-
-            var finalString=new String(stringChar);
-            Console.WriteLine(finalString);
-
-            number--;
-        }
+        Console.Write("Enter an Amount: ");
+        int num = Convert.ToInt32(Console.ReadLine());
+        VendingMachine.countCurrency(num);
+        
     }
 }   
     
